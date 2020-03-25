1f5dcf627bae347eb2f02cb98423a9d59e92dd92
Spencer Moody <spencer.moody@outlook.com>
2020-03-25 14:51:23 -0700

    Add radio buttons to survey

--- survey/js/scripts.js
+++ survey/js/scripts.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var beverage = $("#beverage").val();
     var favoriteColor = $("#color").val();
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    
+    }
   });
 }
