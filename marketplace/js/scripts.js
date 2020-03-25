1bbc6f73a457041ffe6ce27e944f7cd72121300a
Spencer Moody <spencer.moody@outlook.com>
2020-03-25 16:19:51 -0700

    Add receipt

---
+++ marketplace/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("button#submit").submit(function(event) {
+  
+    var inputName = $("input#inputName").val();
+    var inputAddressLine1 = $("input#inputAddressLine1").val();
+    var inputAddressLine2 = $("input#inputAddressLine2").val();
+
+    $("#")
+
+  $("#confirmation").text(inputAddressLine1);
+  $("#confirmation").text(inputAddressLine2);
+  
+  event.preventDefault();
+  });
+  
+});
